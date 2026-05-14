14edf8009fd42c97885a8d7050bee2939f2012de
David <davidfelipecarrillogarzon@gmail.com>
2026-05-14 06:00:54 -0500

    fix: the version of the program is fixed and spelling errors are corrected

--- sublimAK/src/App.java
+++ sublimAK/src/App.java
@@ -50,7 +50,7 @@ public class App extends JFrame{
                 case 4:
                     System.exit(0);
                 case 5:
-                    JOptionPane.showMessageDialog(this, "La version De Este Programa Es La 1.0.5\n\nNuevos Arreglos De Esta Versión:\n\n1. Se Añade Función De Editar Productos De Stock.\n\n2. Arreglos Internos");
+                    JOptionPane.showMessageDialog(this, "La version De Este Programa Es La 1.0.6\n\nNuevos Arreglos De Esta Versión:\n\n1.Se Agrega Opción De Eliminar Productos");
                     continue;
             }
    } 
