f6ed31d894dbcbfdf6888aa920ab672de263824f
David <davidfelipecarrillogarzon@gmail.com>
2026-05-15 15:30:56 -0500

    feat: add payment method selection in sales record

--- sublimAK/src/RegistrarVentas.java
+++ sublimAK/src/RegistrarVentas.java
@@ -112,22 +112,26 @@ public class RegistrarVentas {
                 
 
     private void finalizarventa(int indice, int cantidad, double totalVenta){
-                    app.cuenta.dinero += (float) totalVenta;
-                    app.cuenta.historial.add(new CuentaEmpresa.Movimiento(
-                        app.cuenta.contadorMovimientos++,
-                        "Venta - " + app.inventario.nombres[indice],
-                        (float) totalVenta
-                    ));
-                    app.inventario.stocks[indice] -= cantidad;
+        String[] botonesMedioPago = {"Efectivo", "Nequi"};
+        int eleccionMedioPago = JOptionPane.showOptionDialog(app, "¿Porque Medio Se Realizó El Pago?", "Medio De Pago", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, botonesMedioPago, botonesMedioPago[0]);
+        if(eleccionMedioPago == 0) {
+            app.cuenta.dinero += (float) totalVenta;
+        }else if(eleccionMedioPago == 1){app.cuenta.dineroDigital += (float) totalVenta;}
+        else{return;}
+        String medioPago = (eleccionMedioPago == 0) ? "Efectivo" : "Nequi";
+        app.cuenta.historial.add(new CuentaEmpresa.Movimiento(
+        app.cuenta.contadorMovimientos++,
+        "Venta " + medioPago + " - " + app.inventario.nombres[indice],
+        (float) totalVenta));
+        app.inventario.stocks[indice] -= cantidad;
 
-                    //Mensaje de registro de ventas
-                    JOptionPane.showMessageDialog(ventana,"Venta Registrada\nProducto: " + app.inventario.nombres[indice] + "\nCantidad: " + cantidad + "\nTotal: $" + totalVenta);
 
-                    //Guardado de datos
-                    Datos.guardarInventario(app.inventario);        
-                    Datos.guardarCuenta(app.cuenta);     
-                    Datos.guardarHistorial(app.cuenta); 
+        //Mensaje de registro de ventas
+        JOptionPane.showMessageDialog(ventana,"Venta Registrada\nProducto: " + app.inventario.nombres[indice] + "\nCantidad: " + cantidad + "\nTotal: $" + totalVenta);
+
+        //Guardado de datos
+        Datos.guardarInventario(app.inventario);        
+        Datos.guardarCuenta(app.cuenta);     
+        Datos.guardarHistorial(app.cuenta); 
     }
-    
-     
 }
